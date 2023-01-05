0c007dc4bb6f295d4c6650de9559cae8e63a2e0f
devgenx <sebastian110420@gmail.com>
2023-01-05 13:03:16 -0500

    Updated css and script

--- Phase-1-proj/index.js
+++ Phase-1-proj/index.js
@@ -2,13 +2,12 @@ const sliderContainer = document.querySelector(".slider-container");
 const slider = document.querySelector(".slider");
 
 let i = 1;
-
 const fetchTrendingData = async () => {
   const response = await fetch(
     "https://api.coingecko.com/api/v3/search/trending"
   );
-  const data = await response.json();
 
+  const data = await response.json();
   data.coins.forEach((item) => handleTrendData(item.item));
 };
 
@@ -20,50 +19,44 @@ const handleTrendData = (data) => {
 
   const contentDiv = document.createElement("div");
   contentDiv.className = "content";
-
   tokenImg.src = data.large;
   tokenName.textContent = ` ${i++}. ${data.name} (${data.symbol})`;
-
   contentDiv.appendChild(tokenImg);
   contentDiv.appendChild(tokenName);
-
   slider.appendChild(contentDiv);
-
   sliderContainer.appendChild(slider);
 };
 
+//Clicks on the Add token button, popUp form pops up, closes on the X button.
+const addTokenBtn = document.querySelector(".btn");
+const popUpBg = document.querySelector(".popup-bg");
+const popUpClose = document.querySelector(".popup-close");
 
-//Clicks on the Add tocken button, popUp form pops up, closes on the X button. 
-
-const addTokenBtn = document.querySelector('.btn')
-const popUpBg = document.querySelector('.popup-bg')
-const popUpClose = document.querySelector('.popup-close')
-addTokenBtn.addEventListener('click', () => {
+addTokenBtn.addEventListener("click", () => {
   //console.log('click')
-  popUpBg.classList.add('bg-active')
-})
+  popUpBg.classList.add("bg-active");
+});
 
-popUpClose.addEventListener('click', () => {
-  popUpBg.classList.remove('bg-active')
-})
+popUpClose.addEventListener("click", () => {
+  popUpBg.classList.remove("bg-active");
+});
 
 // Submits a form with new coins and appends it to the slide.
-const cryptoForm = document.querySelector('.crypto-form')
-cryptoForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  console.log('submit')
-    const formName = document.querySelector('#name').value
-    const formSymbol = document.querySelector('#symbol').value
-    const formImage = document.querySelector('#img').value
-    const token = {
-      name: formName,
-      symbol: formSymbol,
-      large: formImage
-    }
-    handleTrendData(token)
-    popUpBg.classList.remove('bg-active')
-    cryptoForm.reset()
-})
+const cryptoForm = document.querySelector(".crypto-form");
+cryptoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formName = document.querySelector("#name").value;
+  const formSymbol = document.querySelector("#symbol").value;
+  const formImage = document.querySelector("#img").value;
+
+  const token = {
+    name: formName,
+    symbol: formSymbol,
+    large: formImage,
+  };
+  handleTrendData(token);
+  popUpBg.classList.remove("bg-active");
+  cryptoForm.reset();
+});
 
 fetchTrendingData();
-
